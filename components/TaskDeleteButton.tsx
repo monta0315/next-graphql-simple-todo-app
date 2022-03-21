4556779d78bc0f5de44d9475aaea7473debc4538
monta0315 <monta0315@docomo.ne.jp>
2022-03-21 19:43:05 +0900

    done

---
+++ components/TaskDeleteButton.tsx
@@ -0,0 +1,45 @@
+import { gql, useMutation } from "@apollo/client";
+import { SmallCloseIcon } from "@chakra-ui/icons";
+import { Box, IconButton } from "@chakra-ui/react";
+import React from "react";
+import { AllTasksQuery } from "./TaskList";
+
+const DeleteTaskMutation = gql`
+  mutation DeleteTask($id: Int!) {
+    deleteTask(id: $id) {
+      id
+    }
+  }
+`;
+
+const TaskDeleteButton: React.FC<{ taskId: number }> = ({ taskId }) => {
+  const [deleteTask, { error }] = useMutation(DeleteTaskMutation, {
+    refetchQueries: [AllTasksQuery],
+  });
+
+  const handleClick = () => {
+    deleteTask({
+      variables: {
+        id: taskId,
+      },
+    });
+  };
+
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <Box>
+      <IconButton
+        aria-label="tasl delete button"
+        icon={<SmallCloseIcon />}
+        variant="ghost"
+        color="gray.200"
+        _hover={{ bg: "none", color: "gray" }}
+        _focus={{ boxShadow: "none" }}
+        onClick={() => handleClick()}
+      />
+    </Box>
+  );
+};
+
+export { TaskDeleteButton };
--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -1,5 +1,6 @@
-import { gql, useQuery } from "@apollo/client";
-import { Checkbox, List, ListItem } from "@chakra-ui/react";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { Checkbox, Flex, List, ListItem } from "@chakra-ui/react";
+import { TaskDeleteButton } from "./TaskDeleteButton";
 
 export const AllTasksQuery = gql`
   query {
@@ -11,19 +12,58 @@ export const AllTasksQuery = gql`
   }
 `;
 
+const UpdateTaskMutation = gql`
+  mutation UpdateTask($id: Int!, $title: String!, $done: Boolean!) {
+    updateTask(id: $id, title: $title, done: $done) {
+      id
+    }
+  }
+`;
+
+type Task = {
+  id: number;
+  title: string;
+  done: boolean;
+};
+
 const TaskList: React.FC = () => {
   const { data, loading, error } = useQuery(AllTasksQuery);
+  const [updateTask, mutation] = useMutation(UpdateTaskMutation, {
+    refetchQueries: [AllTasksQuery],
+  });
+
+  const handleCheckBoxClick = (task: Task) => {
+    updateTask({
+      variables: {
+        id: task.id,
+        title: task.title,
+        done: !task.done,
+      },
+    });
+  };
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
+  if (mutation.error) return <p>Error: {mutation.error.message}</p>;
+
+  const tasks = [...data.tasks].sort((a: Task, b: Task) => b.id - a.id);
 
   return (
     <List>
-      {data.tasks.map((task: { id: string; title: string; done: boolean }) => (
+      {tasks.map((task: { id: number; title: string; done: boolean }) => (
         <ListItem key={task.id}>
-          <Checkbox colorScheme="teal" isChecked={task.done}>
-            {task.title}
-          </Checkbox>
+          <Flex justify="space-between">
+            <Checkbox
+              colorScheme="teal"
+              isChecked={task.done}
+              onChange={() => {
+                handleCheckBoxClick(task);
+              }}
+            >
+              {task.title}
+            </Checkbox>
+            <TaskDeleteButton taskId={task.id} />
+          </Flex>
         </ListItem>
       ))}
     </List>
